f1ebe7911b5cca1adb5ebbb25d4dfe24c8e2bc2f
zhangfanghui <zhangfanghui>
2017-12-20 13:18:56 +0800

    toolbar

--- README.md
+++ README.md
@@ -1,4 +1,5 @@
 "# android" 
 1.SocketServer-DeskStop  include DeskStop Server and DeskStop client, Android App
 2.Mp3Player Play Mp3 
-3.Weather 
+3.Weather
+4.Toolbar (set navigation bar color) 
---
+++ Toolbar/app/src/main/java/com/lst/burns/toolbar/MainActivity.java
@@ -0,0 +1,78 @@
+package com.lst.burns.toolbar;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.support.v7.widget.Toolbar;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.widget.Toast;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        // App Logo
+        toolbar.setLogo(R.mipmap.ic_launcher);
+        // Title
+        toolbar.setTitle("My Title");
+        // Sub Title
+        toolbar.setSubtitle("Sub title");
+        //toolbar.setNavigationIcon(R.drawable.ab_android);
+        //toolbar.setOnMenuItemClickListener(onMenuItemClick);//无效
+        setSupportActionBar(toolbar);
+        //Navigation Icon 要設定在 setSupoortActionBar 后才有作用
+        // 否則會出現 back button ,实际测试也 没有问题
+        toolbar.setNavigationIcon(R.drawable.ab_android);
+        // Menu item click 的監聽事件一樣要設定在 setSupportActionBar后 才有作用
+        toolbar.setOnMenuItemClickListener(onMenuItemClick);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu_main, menu);
+        return true;
+    }
+    /*
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        //noinspection SimplifiableIfStatement
+        if (id == R.id.action_settings) {
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+    */
+
+    private Toolbar.OnMenuItemClickListener onMenuItemClick = new Toolbar.OnMenuItemClickListener() {
+        @Override
+        public boolean onMenuItemClick(MenuItem menuItem) {
+            String msg = "";
+            switch (menuItem.getItemId()) {
+                case R.id.action_edit:
+                    msg += "Click edit";
+                    break;
+                case R.id.action_share:
+                    msg += "Click share";
+                    break;
+                case R.id.action_settings:
+                    msg += "Click setting";
+                    break;
+            }
+
+            if(!msg.equals("")) {
+                Toast.makeText(MainActivity.this, msg, Toast.LENGTH_SHORT).show();
+            }
+            return true;
+        }
+    };
+}
